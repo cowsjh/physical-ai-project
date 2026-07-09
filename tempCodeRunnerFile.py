f689063021c39a615418512373c114aed60cc194
cowsjh <ef70024@gmail.com>
2026-07-09 22:26:30 +0900

    final

--- tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-eshold1', 'edge')
-        th2 = cv2.getTrackbarPos('threshold2', 'edge')
--- week1/main.py
+++ week1/main.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import matplotlib
+import time
+from collections import deque
 
-
-def capCam () :
+def capture_cam () :
     cap = cv2.VideoCapture(0)
     
     if cap.isOpened() :
@@ -19,8 +20,6 @@ def capCam () :
         cap.release()
         return None
 
-def nothing(x):
-    pass
 
 def grayscale(frame):
     return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -33,45 +32,60 @@ def preprocess(frame, th1, th2, sigma=1):
         blur = g_blur(gray.copy(),sigma)
         canny = cv2.Canny(blur.copy(), th1, th2, apertureSize = 5)
 
-        cv2.imshow('Grayscale',gray)
-        cv2.imshow('Gaussian',blur)
-        cv2.imshow('Gaussian',canny)
+        canny_c = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
 
-def showDis (cap):
+        return np.hstack((frame,canny_c))
+        
+
+
+def show_display (cap):
 
     if not cap :
-        print(f"showDis : Camera가 연결 되지 않았습니다.")
-        cap.release()
+        print(f"show_display : Camera가 연결 되지 않았습니다.")
         return None
 
     cv2.namedWindow('edge')
 
-    cv2.createTrackbar('threshold1','edge',2000,5000,nothing)
-    cv2.createTrackbar('threshold2','edge',2000,5000,nothing)
+    cv2.createTrackbar('threshold1','edge',2000,5000,lambda x :None)
+    cv2.createTrackbar('threshold2','edge',2000,5000,lambda x :None)
 
+    start_time = time.time()
+    p_t = start_time
+    fps_per_frames = deque(maxlen=30)
 
     while True:
         ret,frame = cap.read()
-
+        c_t = time.time()
         if not ret :
             print("프레임이 존재 하지 않습니다.")
             break
         
-        
         th1 = cv2.getTrackbarPos('threshold1', 'edge')
         th2 = cv2.getTrackbarPos('threshold2', 'edge')
 
-        # cv2.imshow('Camera',frame)
-        preprocess(frame,th1,th2)
+        ## set FPS
+        f_t = c_t - p_t
+        p_t = c_t
+        fps = 1.0/f_t
+        # print('fps: ' + str(fps))
+        
+        fps_per_frames.append(fps)
+        avg_fps = np.mean(fps_per_frames)
+        # print(len(fps_per_frames))
+        # print('avg fps: ' + str(avg_fps))
+
+        result = preprocess(frame,th1,th2)
+        cv2.putText(result,f"FPS: {avg_fps:.1f}",(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0))
+        cv2.imshow('Result',result)
+        
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
+        
     cap.release()
     cv2.destroyAllWindows()
 
 
-
 if __name__ == "__main__" :
-    cap = capCam()
-    showDis(cap)
+    cap = capture_cam()
+    show_display(cap)
